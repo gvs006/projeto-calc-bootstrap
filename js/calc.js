6627ecf28cb9ec5fb5746cddb46d0ee61619b95e
gvs006 <40977112+gvs006@users.noreply.github.com>
2022-08-02 09:51:09 -0300

    create repo

---
+++ js/calc.js
@@ -0,0 +1,34 @@
+function insert(num) {
+    var numero = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('res').innerHTML = "";
+}
+
+function back() {
+    var res = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = res.substring(0, res.length - 1)
+}
+
+function calcular() {
+    var res = document.getElementById('res').innerHTML;
+    if (res) {
+        document.getElementById('res').innerHTML = eval(res);
+    }
+    else {
+        if (!res) {
+            errorAlert()
+        }
+    }
+
+}
+
+function errorAlert() {
+    Swal.fire(
+        'Nada para calcular!',
+        'Insira uma função de cálculo!',
+        'error'
+    )
+}
